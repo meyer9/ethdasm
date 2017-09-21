79acaf2de6177d9eb29cfe5ea6ef5194e53ccf81
Julian Meyer <julianmeyer2000@gmail.com>
2017-09-20 21:01:28 -0700

    Added some function definitions

--- contract.py
+++ contract.py
@@ -34,15 +34,48 @@ class ContractLine():
         return repr(self.instruction)
 
 class Output():
+    """
+    Represents the output of an instruction. Also can be considered
+    a single value on the stack. Can be used.
+    """
     def __init__(self, value):
         self.value = value
         self.used = False
     def use(self):
+        """
+        Use the value on the stack. Once used, this value should not be
+        used again.
+        """
         self.used = True
     def __str__(self):
         return self.value
 
+
+class ContractBlock:
+    
+    args_needed: int
+    lines: List[ContractLine]
+    name: str
+    indentation_level: int
+
+    def __init__(self, name):
+        self.args_needed = 0
+        self.lines = []
+        self.name = name
+        self.indentation_level = 1
+    
+    def add_line(self, line: ContractLine):
+        self.lines.append(line)
+    
+    def __str__(self):
+        return "def {}({}):".format(self.name, ", ".join(map(lambda x: "arg" + str(x), range(self.args_needed))))
+
+
 class Contract():
+    """
+    Represents an entire Ethereum contract. Keeps track of symbols,
+    parses opcodes into pseudo-code, and simplifies the pseudo-code.
+    """
     line_blocks: List[List[ContractLine]]
     blocks: List[Block]
 
@@ -55,9 +88,10 @@ class Contract():
         self.symbols = []
         self.line_blocks = []
         self._symbolIdx = 0
+        self.functions = {}
 
     def get_stack_args(self, block_idx: int) -> Output:
-        block = self.line_blocks[block_idx]
+        block = self.line_blocks[block_idx].lines
         for line_num in range(len(block) - 1, -1, -1):
             line = block[line_num]
             if 'DUP' in line.instruction.name:
@@ -86,13 +120,16 @@ class Contract():
                 if not var.used:
                     var.use()
                     return var
-        return Output('arg')
+        return None
 
     def parse(self) -> List[List[ContractLine]]:
         self.line_blocks = []
         block_idx = 0
+        func_num = 0
         for block in self.blocks:
-            line = []
+            line = ContractBlock("func" + str(func_num))
+            func_num += 1
+            self.functions[block.instructions[0].address] = line
             self.line_blocks.append(line)
             instr_idx = 0
             for operation in block.instructions:
@@ -101,29 +138,31 @@ class Contract():
                 if operation.arguments:
                     in_variables = operation.arguments
                 else:
-                    arg = 0
-                    while arg < operation.instruction.removed:
+                    for i in range(operation.instruction.removed):
                         a = self.get_stack_args(block_idx)
+                        if a is None:
+                            a = Output('arg' + str(line.args_needed))
+                            line.args_needed += 1
                         in_variables.append(a)
-                        arg += 1
                 for i in range(operation.instruction.added):
                     out_variables.append(Output('var' + str(self._symbolIdx)))
                     self._symbolIdx += 1
                 # if operation.
                 instruction = ContractLine(address=operation.address, assign_to=out_variables,
                                            instruction=operation.instruction, args=in_variables)
-                line.append(instruction)
+                line.add_line(instruction)
                 # self._symbolIdx += 1
                 instr_idx += 1
             # for swap op codes, we have to wait until after parsing to remove them
 
             idx = 0
-            while idx < len(line):
-                if 'SWAP' in line[idx].instruction.name or 'DUP' in line[idx].instruction.name:
-                    del line[idx]
+            while idx < len(line.lines):
+                if 'SWAP' in line.lines[idx].instruction.name or 'DUP' in line.lines[idx].instruction.name:
+                    del line.lines[idx]
                     idx -= 1
                 idx += 1
             block_idx += 1
+        self.line_blocks[0].name = 'main'
         return self.line_blocks
 
 def main():
@@ -131,9 +170,9 @@ def main():
         contract = Contract(contract.read())
         blocks = contract.parse()
         for lines in blocks:
-            print('===============BLOCK==============')
-            for line in lines:
-                print('[{0}] {1}'.format(hex(line.address), str(line)))
-
+            print(str(lines))
+            for line in lines.lines:
+                print("\t" * lines.indentation_level + '{1}'.format(hex(line.address), str(line)))
+            print()
 if __name__ == '__main__':
     main()
